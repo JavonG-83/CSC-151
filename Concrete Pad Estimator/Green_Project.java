9ede8117423f39f329fb887f2dea5140b18c4305
JavonG-83 <greenj8783@student.faytechcc.edu>
2026-03-25 18:25:34 +0000

    shutting down shop for now

--- Concrete Pad Estimator/Green_Project.java
+++ Concrete Pad Estimator/Green_Project.java
@@ -3,13 +3,13 @@
 import java.util.Scanner;
 public class Green_Project{ public static void main(String[] args){ Scanner input = new Scanner(System.in);
 
-//Declare
-String file = "GreenJ_ConPadEsti.csv"
+//DECLARE VARIABLES
+String line, file = "GreenJ_ConPadEsti.csv";
 String name, location;
 double length, width, thickness, volume;
 int workers;
 
-
-
+//RECEIVE Basic input and info
+// name, company, location, length, width, thickness, workers, total labor hours, 
 input.close();
 }}
